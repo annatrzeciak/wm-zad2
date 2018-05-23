6fb121b991da67b654c39d92d8855027a64d9b4a
Anna <trzeciakanna@hotmail.com>
2018-05-23 21:57:06 +0200

    files panel and RWD

--- js/script.js
+++ js/script.js
@@ -1,6 +1,76 @@
-$('a').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $(this).attr('href')).offset().top
-    }, 500);
-    return false;
+$(document).ready(function() {
+
+
+  if ($(window).width() > 991) {
+    panelPadding()
+    filesPadding()
+  } else {
+    containerPadding();
+    containerHeight();
+  }
+
+
+});
+
+$(window).resize(function() {
+
+  if ($(window).width() > 991) {
+    panelPadding()
+    filesPadding()
+  } else {
+    containerPadding();
+    containerHeight();
+
+  }
+});
+
+
+function panelPadding() {
+  var panels = $('.panel');
+  panels.each(function(index, panel) {
+    panel.style.paddingLeft = ($(window).width() - $('.container').width() - 30) / 2 + 'px';
+  });
+}
+
+function filesPadding() {
+  var filesContent = $('.faq-article .files-content');
+  filesContent.each(function(index, item) {
+    item.style.paddingRight = ($(window).width() - $('.container').width() - 30) / 2 + 'px ';
+  })
+}
+
+function containerPadding() {
+  var filesContent = $('.faq-article .files-content');
+  filesContent.each(function(index, item) {
+    item.style.paddingRight = ($(window).width() - $('.container').width() - 30) / 2 + 'px ';
+    item.style.paddingLeft = ($(window).width() - $('.container').width() - 30) / 2 + 'px ';
+  });
+
+  var panels = $('.panel');
+  panels.each(function(index, panel) {
+    panel.style.paddingLeft = ($(window).width() - $('.container').width() - 30) / 2 + 'px ';
+  });
+}
+
+function containerHeight() {
+  if ((parseInt($('#faq').height())-20) < (parseInt($('#files').height()))) {
+    var faq = $('#faq');
+    faq.each(function(index, item) {
+      item.style.minHeight = ($('#files').height()) + 'px';
+    });
+  } else {
+    var filesContent = $('#files');
+    filesContent.each(function(index, item) {
+      item.style.minHeight = $('#faq').height() +20+ 'px';
+    });
+  }
+}
+$(document).ready(function() {
+  $('.panel-collapse').on('show.bs.collapse', function() {
+    $(this).parent('.panel').addClass('active');
+  });
+
+  $('.panel-collapse').on('hide.bs.collapse', function() {
+    $(this).parent('.panel').removeClass('active');
+  });
 });
